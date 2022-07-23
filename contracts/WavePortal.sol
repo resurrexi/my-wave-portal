8264f574704d6ad02a4b9d0070abea3f7e3316f9
Liquan Yang <yang.liquan87@gmail.com>
2022-07-23 15:17:09 -0400

    WIP: store messages on blockchain

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -1,11 +1,20 @@
 // SPDX-License-Identifier: UNLICENSED
 
+// solhint-disable-next-line
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
 contract WavePortal {
   uint256 totalWaves;
+
+  event NewWave(address indexed from, uint256 timestamp, string message);
+
+  struct Wave {
+    address waver;
+    string message;
+    uint256 timestamp;
+  }
 
   constructor() {
     console.log("Yo yo, I am a contract and I am smart");
